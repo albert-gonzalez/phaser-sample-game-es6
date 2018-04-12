deff8ac5de722753d9583ff992dcda1093a0ed56
Pau Obrador <pau.obrador@atrapalo.com>
2018-04-12 15:27:12 +0200

    [ADD] config

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+import Phaser from 'phaser/dist/phaser.min';
+
+export default {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: true
+    }
+  }
+};
